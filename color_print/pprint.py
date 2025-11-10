734476d278c2422e39925af381f07a4c109d137e
chirag <chiraggujar21@gmail.com>
2025-11-10 18:03:26 +0000

    Added parameter (end=)

--- color_print/pprint.py
+++ color_print/pprint.py
@@ -2,7 +2,7 @@ from os import system
 # Support for some Not-Effective terminal color Scenarios
 system("")
 
-def pprint(text, color="default"):
+def pprint(text, color="default", end_with="\n"):
     colors = {
         "default": 39,      # Default color
         "red": 31,
@@ -17,8 +17,8 @@ def pprint(text, color="default"):
         if color not in list(colors.keys()):
             pprint(f">> Not A PreDefied Color, Selecting Deafult", "red") 
         color_code = colors.get(color, 39)
-        print(f"\033[{color_code}m{text}\033[0m")
+        print(f"\033[{color_code}m{text}\033[0m", end=end_with)
     elif isinstance(color,  int):
-        print(f"\033[38;5;{color_code}m{text}\033[0m")
+        print(f"\033[38;5;{color_code}m{text}\033[0m", end=end_with)
     else:
         raise ValueError
